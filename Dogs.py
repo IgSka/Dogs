b9270de61118a257407b6444d9692f5700d1b13c
IgSka <gaydukovgo@gmail.com>
2024-10-17 19:46:54 +0300

    Устранен баг с пустыми значениями спинбокса. Добавлены значения по умолчанию, равные 300

--- Dogs.py
+++ Dogs.py
@@ -13,7 +13,10 @@ def get_dog_image():
         response = requests.get("https://dog.ceo/api/breeds/image/random")
         response.raise_for_status()
         data = response.json()
-        return data["message"]
+        print(data)
+        print(data['status'])
+        print (data['message'])
+        return data['message']
     except Exception as e:
         mb.showerror("Ошибка", f"Возникла ошибка при запросе к API  {e}")
         return None
@@ -67,13 +70,14 @@ width_label = ttk.Label(text="Ширина:")
 width_label.pack(side="left", padx=(10,0))
 width_spinbox=ttk.Spinbox(from_=200,to=500,increment=50, width=5)
 width_spinbox.pack(side="left", padx=(0,10))
+width_spinbox.set(300)
 
 
 height_label = ttk.Label(text="Высота:")
 height_label.pack(side="left", padx=(10,0))
 height_spinbox = ttk.Spinbox(from_=200,to=500,increment=50, width=5)
 height_spinbox.pack(side="left", padx=(0,10))
-
+height_spinbox.set(300)
 
 
 top_level_window = Toplevel(window)
